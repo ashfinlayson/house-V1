d3d6ed2f2c881b63c3c42f22363d718d61e76086
Ashley Finlayson <ashley.finlayson@iwantgreatcare.org>
2014-04-02 23:33:43 +0100

    Added home controller

---
+++ house-v1/protected/controllers/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+class HomeController extends Controller
+{
+    public $defaultAction = 'index';
+    /**
+     * This is the default 'index' action that is invoked
+     * when an action is not explicitly requested by users.
+     */
+    public function actionIndex()
+    {
+        $this->layout = '//layouts/main';
+        // renders the view file 'protected/views/site/index.php'
+        // using the default layout 'protected/views/layouts/main.php'
+        $this->render('index');
+    }
+    
+}
