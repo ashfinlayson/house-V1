80e03082dabb6124e594d80654d6b3e29329433b
Ashley Finlayson <ashley.finlayson@iwantgreatcare.org>
2014-04-02 23:33:09 +0100

    Added site controller

--- house-v1/protected/controllers/SiteController.php
+++ house-v1/protected/controllers/SiteController.php
@@ -31,7 +31,6 @@ class SiteController extends Controller
         // renders the view file 'protected/views/site/index.php'
         // using the default layout 'protected/views/layouts/main.php'
         $this->render('index');
-        d::d(getenv('APPLICATION_ENV'));
     }
 
     /**
